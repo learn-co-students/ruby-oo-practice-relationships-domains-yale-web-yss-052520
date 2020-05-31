b69c585a8a20feb36560d1e9f34f14239a991951
zayzay500 <lindsay.pierce@yale.edu>
2020-05-31 10:04:30 -0400

    Listing and Guest class deliverables complete

--- app/models/airbnb/listing.rb
+++ app/models/airbnb/listing.rb
@@ -12,7 +12,27 @@ class Listing
         self.class.all << self
     end
 
+    def trips
+        Trip.all.select {|trip| trip.listing == self}
+    end
+
+    def guests
+        trips.map {|trip| trip.guest}
+    end
+
+    def trip_count
+        trips.count 
+    end
+
     def self.all
         @@all
+    end
+
+    def self.find_all_by_city(city)
+        self.all.select {|listing| listing.city == city}
+    end
+
+    def self.most_popular
+        self.all.max {|a, b| a.trip_count <=> b.trip_count}
     end
 end

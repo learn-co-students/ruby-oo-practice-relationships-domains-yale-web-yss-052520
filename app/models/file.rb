b89da1cc260d31aa9bdc5760f384148f17a53651
chad <chad.palmer@yale.edu>
2020-06-01 10:02:41 -0400

    Done.

--- app/models/file.rb
+++ app/models/file.rb
@@ -1,1 +1,449 @@
-# create files for your ruby classes in this directory
+# airbnb
+
+class Listing
+    attr_accessor :name, :city
+
+    @@all = []
+
+    def initialize(name, city)
+        @name = name
+        @city = city
+
+        @@all << self
+    end
+
+    def self.all
+        @@all
+    end
+
+    def trips
+        Trip.all.select {|trip| trip.listing == self}
+    end
+
+    def guests
+        trips.map {|trip| trip.guest}.uniq
+    end
+
+    def trip_count
+        trips.count
+    end
+
+    def self.find_all_by_city(city)
+        all.select {|listing| listing.city == city}
+    end
+
+    def self.most_popular
+        all.reduce {|memo, listing| memo.trip_count > listing.trip_count ? memo : listing}
+    end
+end
+
+class Guest
+    attr_accessor :name
+
+    @@all = []
+
+    def initialize(name)
+        @name = name
+
+        @@all << self
+    end
+
+    def self.all
+        @@all
+    end
+
+    def trips
+        Trip.all.select {|trip| trip.guest == self}
+    end
+
+    def listings
+        trips.map {|trip| trip.listing}.uniq
+    end
+
+    def trip_count
+        trips.count
+    end
+
+    def self.pro_traveller
+        all.select {|guest| guest.trip_count > 1}
+    end
+
+    def self.find_all_by_name(name)
+        all.select {|guest| guest.name == name}
+    end
+end
+
+class Trip
+    attr_accessor :date, :listing, :guest
+
+    @@all = []
+
+    def initialize(date, listing, guest)
+        @guest = guest
+        @listing = listing
+        @date = date
+
+        @@all << self
+    end
+
+    def self.all
+        @@all
+    end
+end
+
+# bakery
+
+class Bakery
+    attr_accessor :name
+
+    @@all = []
+
+    def initialize(name)
+        @name = name
+        @@all << self
+    end
+
+    def self.all
+        @@all
+    end
+
+    def ingredients
+        desserts.map {|dessert| dessert.ingredients}.flatten
+    end
+
+    def desserts
+        Dessert.all.select {|dessert| dessert.bakery == self}
+    end
+
+    def average_calories
+        desserts.reduce(0.0) {|sum, dessert| sum + dessert.calories} / desserts.count
+    end
+
+    def shopping_list
+        ingredients.map {|ingredient| ingredient.name}.uniq
+    end
+end
+
+class Dessert
+    attr_accessor :name, :bakery
+
+    @@all = []
+
+    def initialize(name, bakery)
+        @name = name
+        @bakery = bakery
+
+        @@all << self
+    end
+
+    def self.all
+        @@all
+    end
+
+    def ingredients
+        Ingredient.all.select {|ingredient| ingredient.dessert == self}
+    end
+
+    def calories
+        ingredients.reduce(0) {|sum, ingredient| sum + ingredient.calories}
+    end
+end
+
+class Ingredient
+    attr_accessor :name, :calories, :dessert
+
+    @@all = []
+
+    def initialize(name, calories, dessert)
+        @name = name
+        @calories = calories
+        @dessert = dessert
+
+        @@all << self
+    end
+
+    def bakery
+        dessert.bakery
+    end
+
+    def self.all
+        @@all
+    end
+
+    def self.find_all_by_name(string)
+        all.select {|ingredient| ingredient.name.include?(string)}
+    end
+end
+
+# lyft
+
+class Passenger
+    attr_accessor :name
+
+    @@all = []
+
+    def initialize(name)
+        @name = name
+
+        @@all << self
+    end
+
+    def self.all
+        @@all
+    end
+
+    def drivers
+        rides.map {|ride| ride.driver}.uniq
+    end
+
+    def rides
+        Ride.all.select {|ride| ride.passenger == self}
+    end
+
+    def total_distance
+        rides.reduce(0) {|sum, ride| sum + ride.distance}
+    end
+
+    def self.premium_members
+        all.select {|member| member.total_distance > 100}
+    end
+end
+
+class Driver
+    attr_accessor :name
+
+    @@all = []
+
+    def initialize(name)
+        @name = name
+
+        @@all << self
+    end
+
+    def self.all
+        @@all
+    end
+
+    def rides
+        Ride.all.select {|ride| ride.driver == self}
+    end
+
+    def passengers
+        rides.map {|ride| ride.passenger}.uniq
+    end
+
+    def mileage
+        rides.reduce(0) {|sum, ride| sum + ride.distance}
+    end
+
+    def self.mileage_cap(distance)
+        all.select {|driver| driver.mileage > distance}
+    end
+end
+
+class Ride
+    attr_accessor :passenger, :driver, :distance
+
+    @@all = []
+
+    def initialize(passenger, driver, distance)
+        @passenger = passenger
+        @driver = driver
+        @distance = distance
+
+        @@all << self
+    end
+
+    def self.all
+        @@all
+    end
+
+    def self.average_distance
+        all.reduce(0.0) {|sum, ride| sum + ride.distance} / all.count
+    end
+end
+
+# imdb
+
+class Movie
+    attr_accessor :name
+
+    @@all = []
+
+    def initialize(name)
+        @name = name
+
+        @@all << self
+    end
+
+    def self.all
+        @@all
+    end
+
+    def join_class_search(klass)
+        klass.all.select {|member| member.movie == self}
+    end
+
+    def characters
+        joinclass = join_class_search(MovieJoinCharacter)
+        joinclass.map {|member| member.character}
+    end
+
+    def actors
+        characters.map {|char| char.actor}.uniq
+    end
+
+    def self.most_actors
+        all.max_by {|movie| movie.actors.count}
+    end
+end
+
+class MovieJoinCharacter
+    attr_accessor :movie, :character
+
+    @@all = []
+
+    def initialize(movie, character)
+        @movie = movie
+        @character = character
+
+        @@all << self
+    end
+
+    def self.all
+        @@all
+    end
+end
+
+class Show
+    attr_accessor :name
+
+    @@all = []
+
+    def initialize(name)
+        @name = name
+
+        @@all << self
+    end
+
+    def self.all
+        @@all
+    end
+
+    def join_class_search(klass)
+        klass.all.select {|member| member.show == self}
+    end
+
+    def characters
+        joinclass = join_class_search(ShowJoinCharacter)
+        joinclass.map {|member| member.character}
+    end
+
+    def actors
+        characters.map {|character| character.actor}.uniq
+    end
+
+    def on_the_big_screen
+        Movie.all.find {|movie| movie.name == name}
+    end
+end
+
+class ShowJoinCharacter
+    attr_accessor :show, :character
+
+    @@all = []
+
+    def initialize(show, character)
+        @show = show
+        @character = character
+
+        @@all << self
+    end
+
+    def self.all
+        @@all
+    end
+end
+
+class Character
+    attr_accessor :name, :actor
+
+    @@all = []
+
+    def initialize(name, actor)
+        @name = name
+        @actor = actor
+
+        @@all << self
+    end
+
+    def self.all
+        @@all
+    end
+    
+    def join_class_search(klass)
+        klass.all.select {|member| member.character == self}
+    end
+
+    def movies
+        joinclass = join_class_search(MovieJoinCharacter)
+        joinclass.map {|member| member.movie}
+    end
+
+    def shows
+        joinclass = join_class_search(ShowJoinCharacter)
+        joinclass.map {|member| member.show}
+    end
+
+    def appearances
+        movies.count + shows.count
+    end
+
+    def self.most_appearances
+        all.max_by {|character| character.appearances}
+    end
+end
+
+class Actor
+    attr_accessor :name
+
+    @@all = []
+
+    def initialize(name)
+        @name = name
+
+        @@all << self
+    end
+
+    def self.all
+        @@all
+    end
+
+    def characters
+        Character.all.select {|char| char.actor == self}
+    end
+
+    def self.most_characters
+        all.max_by {|actor| actor.characters.count}
+    end
+end
+
+# ExClass
+
+class PasteThis
+    attr_accessor
+
+    @@all = []
+
+    def initialize()
+
+
+        @@all << self
+    end
+
+    def self.all
+        @@all
+    end
+end
+
+# TODO: crowdfunding, gym
--- tools/console.rb
+++ tools/console.rb
@@ -4,4 +4,70 @@ def reload
   load 'config/environment.rb'
 end
 
+#
+
+def airbnb_tests
+  l1 = Listing.new("Cool Place", "Texas")
+  l2 = Listing.new("Good Place", "Maine")
+  l3 = Listing.new("Bad Place", "Texas")
+
+  g1 = Guest.new("Jim")
+  g2 = Guest.new("Chad")
+  g3 = Guest.new("Jeff")
+
+  t1 = Trip.new("3/4", l1, g1)
+  t2 = Trip.new("3/5", l1, g3)
+  t3 = Trip.new("3/6", l2, g1)
+  t4 = Trip.new("3/7", l3, g3)
+  t5 = Trip.new("3/8", l3, g2)
+end
+
+def bakery_tests
+  b1 = Bakery.new("Cool Bakery")
+  b2 = Bakery.new("Neat Bakery")
+
+  d1 = Dessert.new("Choco Cake", b1)
+  d2 = Dessert.new("Vanilla Cake", b1)
+  d3 = Dessert.new("Special Thing", b1)
+  d4 = Dessert.new("Worm Food", b2)
+
+  i1 = Ingredient.new("Flour", 100, d1)
+  i2 = Ingredient.new("Paste", 25, d1)
+  i3 = Ingredient.new("Milk", 300, d1)
+  i4 = Ingredient.new("Prune Juice", 150, d2)
+  i5 = Ingredient.new("Frosting", 50, d2)
+  i6 = Ingredient.new("Salt", 100, d3)
+  i7 = Ingredient.new("Candles", 500, d3)
+  i8 = Ingredient.new("Melon", 125, d4)
+  i9 = Ingredient.new("Ham", 75, d4)
+end
+
+def lyft_tests
+  p1 = Passenger.new("Jeff")
+  p2 = Passenger.new("Chad")
+  p3 = Passenger.new("Jim")
+
+  d1 = Driver.new("Bob")
+  d2 = Driver.new("Rob")
+  d3 = Driver.new("Job")
+
+  r1 = Ride.new(p1, d1, 20)
+  r2 = Ride.new(p1, d2, 10)
+  r3 = Ride.new(p2, d1, 30)
+  r4 = Ride.new(p3, d3, 5)
+  r5 = Ride.new(p3, d3, 100)
+  r6 = Ride.new(p2, d3, 50)
+end
+
+def imdb_tests
+  movie = Movie.new("movie")
+  show = Show.new("show")
+  actor = Actor.new("chad")
+  character = Character.new("superguy", actor)
+  mjc = MovieJoinCharacter.new(movie, character)
+  sjc = ShowJoinCharacter.new(show, character)
+end
+
+#
+
 Pry.start
